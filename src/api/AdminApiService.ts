10c0b4dd5a0ea54ea2cb05cd6c344ec30146a64b
Andrei Riaskov <ryaskov.work@gmail.com>
2018-04-26 01:31:38 +0300

    Add AdminApiService

---
+++ src/api/AdminApiService.ts
@@ -0,0 +1,22 @@
+import * as express from 'express'
+import { ApiService } from './ApiService'
+import * as path from 'path'
+
+export class AdminApiService implements ApiService {
+  namespace = 'admin'
+
+  router: express.Router = express.Router()
+
+  constructor () {
+    this.render = this.render.bind(this)
+    this.setupRoutes()
+  }
+
+  private render (req: express.Request, res: express.Response) {
+    res.sendFile(path.resolve(__dirname, '../../public/index.html'))
+  }
+
+  private setupRoutes () {
+    this.router.get('/*', this.render)
+  }
+}
